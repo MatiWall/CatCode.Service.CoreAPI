66dfe4e7482086ad5a67efd8635c5cc11dd31301
Matias Wallenius <MatiWall@live.dk>
2024-09-17 21:14:50 +0200

    updating to automatically generate keys for storage (not working)

--- core_api/api/etcd.py
+++ core_api/api/etcd.py
@@ -4,10 +4,54 @@ from fastapi import APIRouter, Query
 from fastapi.exceptions import HTTPException
 
 from core_api.etcd.command import run_command
+from core_api.resource.path import resource_path_builder
 from core_api.resource.validation import resource_validation
 from settings import config
 
 router = APIRouter(prefix='/resource')
+
+
+# def get_existing_resource(key):
+#     command = ['etcdctl', 'get', key]
+#     try:
+#         result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+#         return result.stdout
+#     except subprocess.CalledProcessError as e:
+#         print(f"Error: {e.stderr}")
+#         raise ValueError(f'Failed to retrieve resource for key')
+#
+#
+# def update_resource(resource):
+#     path = f'{config.core_api}'
+#
+#     resp = requests.get(path)
+#     if resp.status_code == 200:
+#         response = resp.json()
+#         resource_exists = response.get('exists')
+#         if resource_exists:
+#             current_state = resp.json()['value']
+#             current_state = json.loads(current_state)
+#
+#             if resource == current_state:
+#                 logger.debug(f'State of object did not change, continuing.')
+#                 return
+#
+#         logger.info(f'Resource changed: ')
+#
+#         if resp.status_code == 200:
+#             logger.info(f'Successfully updated resource at {key}')
+#         else:
+#             logger.error(f'Failed to update resource at {key}')
+#     else:
+#         logger.error(f'Failed to read current state of resource {key}')
+@router.post("/")
+def post_resource(resource: dict):
+
+    resource = resource_validation(resource)
+    path = resource_path_builder(resource)
+    command = ['etcdctl', f'--endpoints={config.etcd_host}', 'put', path, json.dumps(resource)]
+    run_command(command)
+    return {"key": path, "value": resource}
 
 
 @router.get('/{path:path}')
@@ -34,15 +78,6 @@ def get_resource(path: str, prefix: bool = Query(default=False)):
     except HTTPException as e:
         raise e
 
-
-@router.post("/{path:path}")
-def post_resource(path: str, resource: dict):
-
-    resource = resource_validation(resource)
-
-    command = ['etcdctl', f'--endpoints={config.etcd_host}', 'put', path, json.dumps(resource)]
-    run_command(command)
-    return {"key": path, "value": resource}
 
 
 @router.delete('/{path:path}')
--- core_api/resource/path.py
+++ core_api/resource/path.py
@@ -1,5 +1,23 @@
+from typing import Optional
 
 
-def resource_path_builder(resource: dict):
-    apigroup, apiversion = resource['apiVersion'].split('/')
-    name = resource['metadata']['name']
+def resource_path_creator(resource: dict):
+    if resource['kind'] == 'BaseSchema':
+        return f'resource/registry/api.catcode.io/baseschema'
+    elif resource['kind'] == 'ResourceDefinition':
+        apigroup = resource['apiVersion'].split('/')[0]
+        resource_group = resource['spec']['group']
+        singular = resource['spec']['names']['singular']
+
+        return f'resource/registry/{apigroup}/resourcedefinition/{resource_group}/{singular}'
+    else:
+        raise ValueError('To be implemented. Resource key')
+        # command = ['etcdctl', f'--endpoints={config.etcd_host}', 'get', path, '--print-value-only']
+        # output = run_command(command)
+        # return f'resource/registry/{apigroup}/resourcedefinition/{resource_group}/{singular}'
+
+def resource_path_builder(
+
+        api_group: Optional[str] = None
+):
+    pass
